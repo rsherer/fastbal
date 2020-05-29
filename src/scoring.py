1ade6422b0e3825f4fc08d276a6a930eb343755c
Robert Sherer <rsherer@gmail.com>
2020-05-29 14:44:11 -0700

    Add scoring module

---
+++ src/scoring.py
@@ -0,0 +1,22 @@
+import numpy as numpy
+
+season_stats_columns = ['ID', 'NAME', 'TEAM', 'RD', 'HOME_AWAY', 'OPPONENT',
+                        'PTS', 'MIN', 'GF', 'A', 'CS', 'PS', 'PE', 'PM', 'GA', 
+                        'SV', 'Y', 'R', 'OG', 'T', 'P', 'KP', 'CRS', 'BC', 
+                        'CL', 'BLK', 'INT', 'BR', 'ELG', 'OGA', 'SH', 'WF']
+
+### to do: create functions for each column that is used to total
+### game by game fantasy points.
+
+def game_points(minutes: int) -> int:
+    if minutes >= 60:
+        return 2
+    elif minutes > 0:
+        return 1
+    else:
+        return 0
+
+assert game_points(65) == 2, game_points(65)
+assert game_points(57) == 1, game_points(57)
+assert game_points(0) == 0, game_points(0)
+assert game_points(45) != 2, game_points(45)
