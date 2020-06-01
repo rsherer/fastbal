fcd3a9df139ebf36683c3ff5f54a982c4615d547
Robert Sherer <rsherer@gmail.com>
2020-06-01 14:46:43 -0700

    add assists function

--- src/scoring.py
+++ src/scoring.py
@@ -1,4 +1,4 @@
-import numpy as numpy
+import numpy as np 
 
 season_stats_columns = ['ID', 'NAME', 'TEAM', 'RD', 'HOME_AWAY', 'OPPONENT',
                         'PTS', 'MIN', 'GF', 'A', 'CS', 'PS', 'PE', 'PM', 'GA', 
@@ -6,9 +6,10 @@ season_stats_columns = ['ID', 'NAME', 'TEAM', 'RD', 'HOME_AWAY', 'OPPONENT',
                         'CL', 'BLK', 'INT', 'BR', 'ELG', 'OGA', 'SH', 'WF']
 
 ### to do: create functions for each column that is used to total
-### game by game fantasy points.
+### game by game fantasy points. Points are calculated from the category 'MIN'
+### onward
 
-def calculate_minutes_points(minutes: int) -> int:
+def points_for_minutes_played(minutes: int) -> int:
     '''Calculate the number of fantasy points for the 'MIN' column for each
     player's games.
     '''
@@ -19,12 +20,19 @@ def calculate_minutes_points(minutes: int) -> int:
     else:
         return 0
 
-def calculate_goals_scored_points(goals: int, position: str) -> int:
-    '''Calculate fantasy points for goals scored. 6 points for a goalkeeper
-    or a defender, 5 points for a midfielder or forward.
+def points_for_goals_scored(goals: int, position: str) -> int:
+    '''Calculate fantasy points for 'GF' column which is goals scored. 
+    6 points for a goalkeeper or a defender, 5 points for a midfielder 
+    or forward.
     '''
     assert position in ['G', 'D', 'M', 'F'], 'Not a valid position'
     if position in ['G', 'D']:
         return 6 * goals
     if position in ['M', 'F']:
         return 5 * goals
+
+def points_for_assists(assists: int) -> int:
+    '''Calculate fantasy points for 'A', which is assists in a match. Each
+    assist or secondary assist during a match is 3 points for the player.
+    '''
+    return assists * 3
