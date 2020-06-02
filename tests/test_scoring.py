16ce8213c335d14f803cfd459a5f29a224a7d0a7
Robert Sherer <rsherer@gmail.com>
2020-06-01 18:37:22 -0700

    add tests through clean sheet

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -2,26 +2,38 @@ import numpy as np
 import unittest
 from src import scoring as sc
 
-class scTest(unittest.TestCase):
+class TestScoring(unittest.TestCase):
     def test_minutes(self):
-        assert sc.points_for_minutes_played(65) == 2, sc.points_for_minutes_played(65)
-        assert sc.points_for_minutes_played(57) == 1, sc.points_for_minutes_played(57)
-        assert sc.points_for_minutes_played(0) == 0, sc.points_for_minutes_played(0)
+        self.assertEqual(sc.points_for_minutes_played(65), 2)
+        self.assertEqual(sc.points_for_minutes_played(57), 1)
+        self.assertEqual(sc.points_for_minutes_played(0), 0)
 
     def test_goals_scored(self):
-        assert sc.points_for_goals_scored(2, 'G') == 12
-        assert sc.points_for_goals_scored(0, 'G') == 0
-        assert sc.points_for_goals_scored(0, 'D') == 0
-        assert sc.points_for_goals_scored(1, 'D') == 6
-        assert sc.points_for_goals_scored(0, 'M') == 0
-        assert sc.points_for_goals_scored(1, 'M') == 5
-        assert sc.points_for_goals_scored(2, 'M') == 10
-        assert sc.points_for_goals_scored(0, 'F') == 0
-        assert sc.points_for_goals_scored(1, 'F') == 5
-        assert sc.points_for_goals_scored(2, 'F') == 10
-        assert sc.points_for_goals_scored(3, 'F') == 15
+        self.assertEqual(sc.points_for_goals_scored(2, 'G'), 12)
+        self.assertEqual(sc.points_for_goals_scored(0, 'G'), 0)
+        self.assertEqual(sc.points_for_goals_scored(0, 'D'), 0)
+        self.assertEqual(sc.points_for_goals_scored(1, 'D'), 6)
+        self.assertEqual(sc.points_for_goals_scored(0, 'M'), 0)
+        self.assertEqual(sc.points_for_goals_scored(1, 'M'), 5)
+        self.assertEqual(sc.points_for_goals_scored(2, 'M'), 10)
+        self.assertEqual(sc.points_for_goals_scored(0, 'F'), 0)
+        self.assertEqual(sc.points_for_goals_scored(1, 'F'), 5)
+        self.assertEqual(sc.points_for_goals_scored(2, 'F'), 10)
+        self.assertEqual(sc.points_for_goals_scored(3, 'F'), 15)
 
     def test_assists(self):
-        assert sc.points_for_assists(0) == 0
-        assert sc.points_for_assists(1) == 3
-        assert sc.points_for_assists(2) == 6
+        self.assertEqual(sc.points_for_assists(0), 0)
+        self.assertEqual(sc.points_for_assists(1), 3)
+        self.assertEqual(sc.points_for_assists(2), 6)
+
+    def test_clean_sheet(self):
+        self.assertEqual(sc.points_for_clean_sheet(0, 'G'), 0)
+        self.assertEqual(sc.points_for_clean_sheet(1, 'G'), 5)
+        self.assertEqual(sc.points_for_clean_sheet(0, 'D'), 0)
+        self.assertEqual(sc.points_for_clean_sheet(1, 'D'), 5)
+        self.assertEqual(sc.points_for_clean_sheet(0, 'M'), 0)
+        self.assertEqual(sc.points_for_clean_sheet(1, 'M'), 1)
+        self.assertEqual(sc.points_for_clean_sheet(0, 'F'), 0)
+        self.assertEqual(sc.points_for_clean_sheet(1, 'F'), 0)
+        self.assertEqual(sc.points_for_clean_sheet(0, 'D'), 0)
+        self.assertEqual(sc.points_for_clean_sheet(2, 'D'), 0)
