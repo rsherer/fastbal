9ef1403d2d48a5e117ef1df7e8404625d1c9d865
Robert Sherer <rsherer@gmail.com>
2020-06-03 13:34:26 -0700

    update raise value errors

--- src/scoring.py
+++ src/scoring.py
@@ -25,13 +25,14 @@ def goals_scored(goals: int, position: str) -> int:
     6 points for a goalkeeper or a defender, 5 points for a midfielder 
     or forward.
     '''
-    assert position in ['G', 'D', 'M', 'F'], 'Not a valid position'
+    if position not in ['G', 'D', 'M', 'F']:
+        raise ValueError(
+    f'{position} is not valid. Positions can only be: "G", "D", "M", or "F"'
+    )
     if position in ['G', 'D']:
         return goals * 6
     elif position in ['M', 'F']:
         return goals * 5
-    # else:
-    #     return 0
 
 def assists(assists: int) -> int:
     '''Calculate fantasy points for 'A', which is assists in a match. Each
@@ -44,6 +45,10 @@ def clean_sheet(clean_sheet: int, position: str) -> int:
     the defense gives up zero goals. A goalkeeper ('G') or a defender ('D')
     will receive 6 points, a midfielder ('M') will receive 1 point.
     '''
+    if position not in ['G', 'D', 'M', 'F']:
+        raise ValueError(
+    f'{position} is not valid. Positions can only be: "G", "D", "M", or "F"'
+    )
     if clean_sheet != 1:
         return 0
     elif position in ['G', 'D']:
@@ -56,11 +61,8 @@ def clean_sheet(clean_sheet: int, position: str) -> int:
 def penalty_save(saves: int) -> int:
     '''Calculate fantasy points for 'PS', penalties saved. Generally
     attributed to goalies, the column exists for all players. 5 points per
-    penalty save.'''
-    # if saves > 0:
-    #     return saves * 5
-    # else:
-    #     return 0
+    penalty save.
+    '''
     return saves * 5
 
 def penalty_earned(earned: int) -> int:
@@ -68,10 +70,6 @@ def penalty_earned(earned: int) -> int:
     fouled in the penalty box, earning a penalty change for their team.
     Two points for each penalty earned.
     '''
-    # if earned > 0:
-    #     return earned * 2
-    # else:
-    #     return 0
     return earned * 2
 
 def penalty_missed(missed: int) -> int:
@@ -79,10 +77,6 @@ def penalty_missed(missed: int) -> int:
     attempt a penalty kick, and miss for whatever reason. Negative two points
     are earned for each penalty miss.
     '''
-    # if missed > 0:
-    #     return missed * -2
-    # else:
-    #     return 0
     return missed * -2
 
 def goal_against(ga: int, position: str) -> int:
@@ -90,6 +84,10 @@ def goal_against(ga: int, position: str) -> int:
     team. For goalkeepers and defenders, a negative point is earned for every
     two goals against.
     '''
+    if position not in ['G', 'D', 'M', 'F']:
+        raise ValueError(
+    f'{position} is not valid. Positions can only be: "G", "D", "M", or "F"'
+    )
     if position in ['G', 'D'] and ga > 1:
         return (ga // 2) * -1
     else:
@@ -99,8 +97,4 @@ def saves(saves: int) -> int:
     '''Calculate fantasy points for 'S', saves made during the match. For 
     goalkeepers, primarily. One point is earned for every three saves.
     '''
-    # if saves > 0:
-    #     return saves // 3
-    # else:
-    #     return 0
     return saves // 3
