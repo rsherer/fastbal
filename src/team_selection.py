e2a6264e172b985d4755ace8e426d083f1e45481
Robert Sherer <rsherer@gmail.com>
2020-08-07 14:44:02 -0700

    functions for processing and updating run block at end

--- src/team_selection.py
+++ src/team_selection.py
@@ -1,5 +1,6 @@
 # this file should have the end result of having a class that can be run
 # with inputs and produce a team
+from typing import List, Tuple, NamedTuple, TypeVar, Dict
 
 import pickle
 import sys
@@ -10,90 +11,115 @@ import numpy as np
 import pandas as pd
 import pulp
 
-import dataprep as dp
+from dataprep import DataPrep
 import scoring_functions as sc
 
-def get_pickled_model(filename: str) -> object:
+M = TypeVar('M')
+D = TypeVar('D')
+
+def get_pickled_model(filename: str) -> M:
     # get the pickled file from the directory and convert to the model
     basepath = '../models/'
     with open(basepath + filename, "rb") as f:
         pickled_model = pickle.load(f)
     return pickled_model
 
-if __name__ == "__main__":
-    # data file locations
-    meta_str = '../data/metadata/week02/mls_player_metadata_all.csv'
-    top_stats_str = '../data/top_stats/week02/player_top_stats_corrected.csv'
-    season_str = '../data/season_stats/week02/mls_player_stats_all.csv'
-    model_locale = 'baseline_rf.pkl'
+def create_player_dict(formatted_data: D, 
+                           filename: str,
+                           game: int) -> Dict[int, Dict[str, np.ndarray]]:
+    '''formatted_data is a dataprep object that has been instantiated
+    with meta, top_stats, and season data files.
+    rd is the round of the season for the match that will be used for
+    predictions and choosing a team.
+    filename is a string for the pickled model used for predicting the 
+    weekly statistics.
 
-    # retrieve pickled model
-    model = get_pickled_model(model_locale)
-
-    # get get prepared datasets
-    dataprep = dp.DataPrep(meta_str, top_stats_str, season_str)
-    week2 = dataprep.get_data_for_predictions(2)
-
-    # the following steps conduct lookups for using the player position to
-    # get the proper object that will be used to calculate the player's
-    # fantasy score predition
-
-    features, _ = dataprep.merge_data()
-    cols = features[features['rd'] == 2].drop(columns=['name', 'rd']).columns
-
-    # we know these positions map to the specific position object
-    positions_mapping = {'goalie': sc.GoalieOrDefender,
-                        'defense': sc.GoalieOrDefender,
-                        'midfield': sc.Midfielder,
-                        'forward': sc.Forward}
+    The output is a dictionary with player ids as keys, and the values are
+    a dictionary with the following keys and values:
+        'vector' - this is the predicted stats in for the player that week
+        'salary' - the player's salary
+        'team' - the player's team
+        'predicted_score' - this is the sum of fantasy points when running
+        the 'vector' value through the respective position scoring rubrik
+        'position' - the player's position, as in 'defense', 'forward', etc
+    '''
+    feat, _ = formatted_data.merge_data()
+    cols = feat[feat['rd'] == game].drop(columns=['name', 'rd']).columns
+    
     # create a comprehension from the cols because the one-hot encoder may
     # change the order of the positions based on the order of IDs in the table
-    position_lookup = {i: positions_mapping[col]
-                       for i, col in enumerate(cols[2:6])}
+    positions_mapping = {'goalie': sc.GoalieOrDefender,
+                         'defense': sc.GoalieOrDefender,
+                         'midfield': sc.Midfielder,
+                         'forward': sc.Forward}
+    score_type_lookup = {i: positions_mapping[col]
+                         for i, col in enumerate(cols[2:6])}    
 
-    for k, v in week2.items():
-        player_score = position_lookup[np.argmax(
+    model = get_pickled_model(filename)
+    week = formatted_data.get_data_for_predictions(game)
+
+    for k, v in week.items():
+        player_score = score_type_lookup[np.argmax(
                 v['vector'][0][1:5]
             )
         ](
             *model.predict(v['vector'])[0]
         )
-        week2[k]['predicted_score'] = player_score.score()
-        week2[k]['position'] = cols[2:6][np.argmax(
+        week[k]['predicted_score'] = player_score.score()
+        week[k]['position'] = cols[2:6][np.argmax(
                 v['vector'][0][1:5])]
-    
-    print(week2)
+    return week
 
-    
-# first step is to choose a game round, get all eligible players for that
-# round, and put them through a pickled model to produce predictions for
-# the week.
+def create_lp_dicts(round_dict: Dict[int, Dict[str, float]]) -> \
+                            Tuple[
+                                Dict[str, Dict[int, str]],
+                                Dict[str, Dict[int, float]],
+                                Dict[str, Dict[int, str]]]:
+    '''Data wrangling with a pandas and dictionary comprehensions to
+    organize data into suitable formats to solve the Linear Programming
+    Problem.
+    Inputs: dict of players and data for the given game.
+    Outputs: dict of player by salaries, dict of players by predicted
+    points, dict of team by players.
+    '''
+    table = pd.DataFrame(round_dict).T
+    table['player_id'] = table.index
+    unique_pos = set(v['position'] for k, v in round_dict.items())
+    unique_team = set(v['team'] for k, v in round_dict.items())
 
+    salaries = {}
+    predicted_points = {}
 
+    for pos in unique_pos:
+        available = table[table['position'] == pos]
+        salary = list(available[['player_id', 'salary']].set_index('player_id').to_dict().values())[0]
+        point = list(available[['player_id', 'predicted_score']].set_index('player_id').to_dict().values())[0]
+        salaries[pos] = salary
+        predicted_points[pos] = point
 
+    team_by_player = {
+        team: {
+            k: v['position'] 
+            for k, v in round_dict.items() if v['team'] == team
+        }
+        for team in unique_team
+    }
 
+    return salaries, predicted_points, team_by_player
 
+def lp_problem:
+    pass
 
+if __name__ == "__main__":
+    # data file locations
+    meta_str = '../data/metadata/week02/mls_player_metadata_all.csv'
+    top_stats_str = '../data/top_stats/week02/player_top_stats_corrected.csv'
+    season_str = '../data/season_stats/week02/mls_player_stats_all.csv'
+    model_locale = 'baseline_rf.pkl'    
 
-# from the MLS Fantasy game rules:
+    # get player dictionary
+    dataprepped = DataPrep(meta_str, top_stats_str, season_str)
+    players = create_player_dict(dataprepped, model_locale, 2)
 
-# For each match, a squad consists of 15 players wtih a max budget of
-# 125 million fantasy dollars.
-#
-# The squad must have:
-#     2 goalkeepers
-#     5 defenders
-#     5 midfielders
-#     3 forwards
-
-# 11 players will be starters, 4 will be on the bench.
-# The following formations are valid:
-
-# Starting (D-M-F)                Bench(D-M-F)
-# 3-5-2                           2-0-1
-# 3-4-3                           2-1-0
-# 4-5-1                           1-0-2
-# 4-4-2                           1-1-1
-# 4-3-3                           1-2-0
-# 5-4-1                           0-1-2
-# 5-3-2                           0-2-1
+    # get LP dictionaries
+    salaries, pred_points, teams = create_lp_dicts(players)
