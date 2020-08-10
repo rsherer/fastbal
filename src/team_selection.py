e8cdadc7d410a288b1fa34de78086a10787a1695
Robert Sherer <rsherer@gmail.com>
2020-08-10 13:31:03 -0700

    functioning optimal team selection

--- src/team_selection.py
+++ src/team_selection.py
@@ -107,8 +107,186 @@ def create_lp_dicts(round_dict: Dict[int, Dict[str, float]]) -> \
 
     return salaries, predicted_points, team_by_player
 
-def lp_problem:
-    pass
+SQAUD_POS_NUM_AVAILABLE = {
+    'goalie': 2,
+    'defense': 5,
+    'midfield': 5,
+    'forward': 3
+}
+
+team_shape_352 = {
+        'goalie': 1,
+        'defense': 3,
+        'midfield': 5,
+        'forward': 2}
+
+subs_shape_352 = {
+         'goalie': 1,
+         'defense': 2,
+         'midfield': 0,
+         'forward': 1}
+
+team_shape_343 = {
+        'goalie': 1,
+        'defense': 3,
+        'midfield': 4,
+        'forward': 3}
+
+subs_shape_343 = {
+         'goalie': 1,
+         'defense': 2,
+         'midfield': 1,
+         'forward': 0}
+
+
+team_shape_451 = {
+        'goalie': 1,
+        'defense': 4,
+        'midfield': 5,
+        'forward': 1}
+
+subs_shape_451 = {
+         'goalie': 1,
+         'defense': 1,
+         'midfield': 0,
+         'forward': 2}
+
+team_shape_442 = {
+        'goalie': 1,
+        'defense': 4,
+        'midfield': 4,
+        'forward': 2}
+
+subs_shape_442 = {
+         'goalie': 1,
+         'defense': 1,
+         'midfield': 1,
+         'forward': 1}
+
+team_shape_433 = {
+        'goalie': 1,
+        'defense': 4,
+        'midfield': 3,
+        'forward': 3}
+
+subs_shape_433 = {
+         'goalie': 1,
+         'defense': 1,
+         'midfield': 2,
+         'forward': 0}
+
+team_shape_541 = {
+        'goalie': 1,
+        'defense': 5,
+        'midfield': 4,
+        'forward': 1}
+
+subs_shape_541 = {
+         'goalie': 1,
+         'defense': 0,
+         'midfield': 1,
+         'forward': 2}
+
+team_shape_532 = {
+        'goalie': 1,
+        'defense': 5,
+        'midfield': 3,
+        'forward': 2}
+
+subs_shape_532 = {
+         'goalie': 1,
+         'defense': 0,
+         'midfield': 2,
+         'forward': 1}
+
+TEAM_SHAPES = [(team_shape_352, subs_shape_352),
+               (team_shape_343, subs_shape_343),
+               (team_shape_451, subs_shape_451),
+               (team_shape_442, subs_shape_442),
+               (team_shape_433, subs_shape_433),
+               (team_shape_541, subs_shape_541),
+               (team_shape_532, subs_shape_532)
+              ]
+
+#constraint for number of players per team
+
+players_team_available = {
+    'atlanta_united_fc': 3,
+    'chicago_fire_fc': 3,
+    'colorado_rapids': 3,
+    'columbus_crew_sc': 3,
+    'dc_united': 3,
+    'fc_cincinnati': 3,
+    'fc_dallas': 3,
+    'houston_dynamo': 3,
+    'inter_miami_cf': 3,
+    'la_galaxy': 3,
+    'los_angeles_fc': 3,
+    'minnesota_united_fc': 3,
+    'montreal_impact': 3,
+    'nashville_sc': 3,
+    'new_england_revolution': 3,
+    'new_york_city_fc': 3,
+    'new_york_red_bulls': 3,
+    'orlando_city_sc': 3,
+    'philadelphia_union': 3,
+    'portland_timbers': 3,
+    'real_salt_lake': 3,
+    'san_jose_earthquakes': 3,
+    'seattle_sounders_fc': 3,
+    'sporting_kansas_city': 3,
+    'toronto_fc': 3,
+    'vancouver_whitecaps_fc': 3
+}
+
+# the salary amount is constrained to 125
+SALARY_CAP = 125
+
+def solve_lp_problem(salaries: Dict[int, Dict[str, int]],
+                     predicted_points: Dict[int, Dict[str, int]],
+                     teams: Dict[str, Dict[int, str]],
+                     team_shape: Dict[str, int],
+                     players_per_team: Dict[str, int],
+                     salary_cap: int) -> float:
+    '''Create and setup the variables for the Linear Programming problem, 
+    and solve it.
+    '''
+    # the player variables are the same whether using the predicted_points
+    # dictionary or the salaries dictionary as reference
+    _player_variables = {k: pulp.LpVariable.dicts(k, v, cat='binary')
+                        for k, v in predicted_points.items()}
+    _team_variables = {k: pulp.LpVariable.dicts(k, v, cat='binary')
+                        for k, v in teams.items()}
+
+    prob = pulp.LpProblem('fastbal', pulp.LpMaximize)
+
+    rewards = []
+    costs = []
+    # create the equations for all player variables by position to show costs, and all player variables by position
+    # to show the predicted points. Will set up a constraint for players by position in order to maximize based
+    # on the team shape chosen.
+    for k, v in _player_variables.items():
+        costs += pulp.lpSum([salaries[k][i] * _player_variables[k][i] for i in v])
+        rewards += pulp.lpSum([predicted_points[k][i] * _player_variables[k][i] for i in v])
+        prob += pulp.lpSum([_player_variables[k][i] for i in v]) <= team_shape[k]
+
+    # create a constraint equation to limit the number of players chose from any single MLS team to 3
+    for k, v in _team_variables.items():
+        prob += pulp.lpSum([_team_variables[k][i] for i in v]) <= players_per_team[k]
+        
+        
+    # this first line sets up the objective, the second line provides the constraint of the salary cap
+    prob += pulp.lpSum(rewards)
+    prob += pulp.lpSum(costs) <= salary_cap
+
+    prob.solve()
+    print(f'Status: {pulp.LpStatus[prob.status]}')
+
+    score = str(prob.objective)
+    for v in prob.variables():
+        score = score.replace(v.name, str(v.varValue))
+
+    return eval(score)
 
 if __name__ == "__main__":
     # data file locations
@@ -123,3 +301,61 @@ if __name__ == "__main__":
 
     # get LP dictionaries
     salaries, pred_points, teams = create_lp_dicts(players)
+
+    _variables = {k: pulp.LpVariable.dicts(k, v, cat='Binary') for k, v in pred_points.items()}
+    _variables_teams = {k: pulp.LpVariable.dicts(k, v, cat='Binary') for k, v in teams.items()}
+
+    results = []
+
+    for starters, _ in TEAM_SHAPES:
+
+        prob = pulp.LpProblem('fastbal', pulp.LpMaximize)
+
+        rewards = []
+        costs = []
+
+        for k, v in _variables.items():
+            costs += pulp.lpSum([salaries[k][i] * _variables[k][i] for i in v])
+            rewards += pulp.lpSum([pred_points[k][i] * _variables[k][i] for i in v])
+            prob += pulp.lpSum([_variables[k][i] for i in v]) <= starters[k]
+
+        for k, v in _variables_teams.items():
+            prob += pulp.lpSum([_variables_teams[k][i] for i in v]) <= players_team_available[k]
+
+        prob += pulp.lpSum(rewards)
+        prob += pulp.lpSum(costs) <= SALARY_CAP
+
+        prob.solve()
+        status = pulp.LpStatus[prob.status]
+        #print(f'Status: {pulp.LpStatus[prob.status]}')
+
+        score = str(prob.objective)
+        for v in prob.variables():
+            score = score.replace(v.name, str(v.varValue))
+
+#        print(eval(score))
+        playing_shape = ''.join((str(v) for v in starters.values()))[1:]
+
+        results.append([playing_shape, status, eval(score)])
+
+    for result in results:
+        print(f'team shape {result[0]} has a score of {result[2]} and status {result[1]}')
+
+    
+
+    # print(solve_lp_problem(salaries,
+    #                        pred_points,
+    #                        teams,
+    #                        team_shape_352,
+    #                        players_team_available,
+    #                        SALARY_CAP))
+
+    # for starters, _ in TEAM_SHAPES:
+    #     score = solve_lp_problem(salaries,
+    #                     pred_points,
+    #                     teams,
+    #                     starters,
+    #                     players_team_available,
+    #                     125)
+    #     playing_shape = ''.join((str(v) for v in starters.values()))[1:]
+    #     print(f'team shape {playing_shape} has a score of {score}')
